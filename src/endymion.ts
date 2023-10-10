fff8058f2e9375a0710c1be7d2393a1e56274fe1
Luigi Senzamici <luigisenzamici@pegaoprofessional.com>
2023-10-10 17:24:59 +0200

    documentation added

--- src/endymion.ts
+++ src/endymion.ts
@@ -26,7 +26,13 @@ class Endymion{
         });
                
     }
-
+    /**
+     * Create action for request to Endymion Browser Application    
+     *
+     * @param actName Action Name, for example: 'create-primitive'
+     * @param actPayload Definition of asset to create
+     * @returns action object
+     */
     private createAction = (actName:string, actPayload:any):any =>{
         var act = {
             name: actName,
@@ -34,15 +40,36 @@ class Endymion{
         };
         return act;
     }
-    public sendAction = (name:string, payload:any)=>{
+    /**
+     * Send action to Endymion Browser Application      
+     * It is a wrapper for createAction and vuplex.postMessage      
+     *
+     * @param name Action Name, for example: 'create-primitive'
+     * @param payload Definition of action
+     * @returns void
+     */
+    public sendAction = (name:string, payload:any):void=>{
         let jsonAction = this.createAction(name, payload);
         this.vuplex.postMessage(jsonAction);
     }
-    public sendActions = (actionArray:any[]) =>{
+    /**
+     * Send multiple actions to Endymion Browser Application        
+     * It is a wrapper for createAction and vuplex.postMessage      
+     * action used is 'multi-action'        
+     * 
+     * @param actionArray Array of actions
+     * @returns void
+     */
+    public sendActions = (actionArray:any[]):void =>{
         var jsonAction = this.createAction('multi-action', actionArray);
         this.vuplex.postMessage(jsonAction);
     }
-
+    /**
+     * Destroy object in Endymion Browser Application       
+     * It is a wrapper for sendAction configured for 'destroy-object'       
+     * 
+     * @param objectId 
+     */
     public destroyObject = (objectId:number)=>{
         this.sendAction(
             'destroy-object',
@@ -51,6 +78,16 @@ class Endymion{
             }
         );
     }
+    /**
+     * Create primitive object in Endymion Browser Application      
+     * It is a wrapper for sendAction configured for 'create-primitive'     
+     * 
+     * @param objectId typeof number
+     * @param primitive typeof primitive
+     * @param position typeof position
+     * @param rotation typeof rotation
+     * @param scale typeof scale
+     */
     public createObject =   (   objectId:number, 
                                 primitive:primitive, 
                                 position:position, 
@@ -68,6 +105,14 @@ class Endymion{
             }
         );
     }
+    /**
+     * Create primitive object in Endymion Browser Application      
+     * It is a wrapper for sendAction configured for 'create-primitive'     
+     * 
+     * @param objectId typeof number
+     * @param source typeof string - url of gltf file
+     * @returns void
+     */
     public importGltf = (objectId: number, source:string):void=>{
         var action = this.sendAction(
             'import-gltf',
@@ -77,8 +122,19 @@ class Endymion{
             }
         );
     }
-    //not usable because cannot set sigle greatness at time
-    //we have must set all greatness at time
+
+    /**
+     * Set a transformation to object       
+     * 
+     * WARNING: not usable because cannot set sigle greatness at time               
+     * we have must set all greatness at time       
+     * 
+     * @param objectId 
+     * @param type typeof TransformType - delta|absolute
+     * @param greatness typeof position|rotation|scale
+     * @param value typeof position|rotation|scale
+     * @returns {objectId:number, action:any}
+     */
     public updateTransform = (objectId: number, 
                         type: TransformType,
                         greatness: TransformGreatness, 
@@ -97,6 +153,13 @@ class Endymion{
             action: action
         }
     }
+    /**
+     * Set color to object      
+     * 
+     * @param objectId 
+     * @param color typeof color - {r:number, g:number, b:number, a:number} in range 0-255
+     * @returns void
+     */
     public setColor = (objectId:number, color:Color):void=>{
         this.sendAction(
             'set-color',
@@ -111,6 +174,11 @@ class Endymion{
             }
         );
     }
+    /**
+     * play haptics on device       
+     * 
+     * @returns void
+     */
     public playHaptic():void{
         this.sendAction(
             'play-haptic',
